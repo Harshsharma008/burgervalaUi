e375856e210c47430fd2972346cb1dae2ba33bd3
Harshsharma008 <harshsharma01012002@gmail.com>
2024-01-11 11:04:28 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import{BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import "./styles/app.scss";
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer"
 import Home from "./components/home/Home";
+import "./styles/home.scss";
+import "./styles/founder.scss";
+import "./styles/menu.scss";
+
 
 function App() {
   return (
@@ -11,6 +16,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         
       </Routes>
+      <Footer/>
     </Router>
   );
 }
